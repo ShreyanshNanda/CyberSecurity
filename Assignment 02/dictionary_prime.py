e9ae35a043806a933dfef06bb5a1b6d2e45001a9
Shreyansh Nanda <68179303+ShreyanshNanda@users.noreply.github.com>
2020-08-29 21:41:09 +0530

    Dictionary Prime Number

---
+++ Assignment 02/dictionary_prime.py
@@ -0,0 +1,16 @@
+d=dict([(2,'Prime'),(3,'Prime'),(4,'Non-Prime'),(5,'Prime'),(6,'Non-Prime'),(7,'Prime')])
+lower=2
+upper=7
+c=0
+for num in range(lower ,upper+1):
+    if (num >1):
+        for i in range(2,num):
+            if(num%i==0):
+                print((num,d.get(num)))
+                del d[num]
+                c=c+1
+                break
+        else:
+            print((num,d.get(num)))
+print(f"Count of Non prime numbers are : {c}")
+print(f"New dictionary is : {d}")
