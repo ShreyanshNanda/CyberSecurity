c1457f43ad349c6bd83ec914cff50d1dbbc83a9d
Shreyansh Nanda <68179303+ShreyanshNanda@users.noreply.github.com>
2020-09-05 14:43:17 +0530

    String is pangram or not

---
+++ Assignment 03/string_pangram.py
@@ -0,0 +1,14 @@
+print("String is pangram or not \n")
+def ispangram(str): 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet: 
+        if char not in str.lower(): 
+            return False
+  
+    return True
+
+str=input("Enter the string : ")
+if(ispangram(str) == True): 
+    print("\nYes") 
+else: 
+    print("\nNo") 
